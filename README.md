fbfd090d37080ba6ff1c06c1eae65c0e90c459f3
xleb1tch <zaharaaa220612@gmail.com>
2022-11-27 17:36:24 +0600

    Beginning and the end

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align=center>PasswordGenerator</h1>
+<br>
+<h3>This is an easy way to come up with a password that has a 99.99 chance that hackers won't crack. This program is written in 30 lines, in python.</h3>
+<br>
+<h5>Thank you for reading this!</h5>
---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+import colorama
+from colorama import *
+import os
+import time
+import sys
+
+colorama.init(autoreset = True)
+
+lower_case = "abcdefghjklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+
+Use_for = lower_case + upper_case + number
+# OldCode
+# length_for_pass = int(input('Password length:'))
+try:
+    length_for_pass = int(input("Password length(61 or more = crash):"))
+    if length_for_pass >= 61:
+    	os.system("cls")
+    	sys.exit()
+except ValueError:
+    print("It's not a number")
+    time.sleep(2)
+    sys.exit()
+
+password = "".join(random.sample(Use_for, length_for_pass))
+
+print(Fore.BLUE + '░█▀▀█ █▀▀█ █▀▀ █▀▀ █   █ █▀▀█ █▀▀█ █▀▀▄    █▀▀▀ █▀▀ █▀▀▄ █▀▀ █▀▀█ █▀▀█ ▀▀█▀▀ █▀▀█ █▀▀█\n░█▄▄█ █▄▄█ ▀▀█ ▀▀█ █▄█▄█ █  █ █▄▄▀ █  █    █ ▀█ █▀▀ █  █ █▀▀ █▄▄▀ █▄▄█   █   █  █ █▄▄▀\n░█    ▀  ▀ ▀▀▀ ▀▀▀  ▀ ▀  ▀▀▀▀ ▀ ▀▀ ▀▀▀     ▀▀▀▀ ▀▀▀ ▀  ▀ ▀▀▀ ▀ ▀▀ ▀  ▀   ▀   ▀▀▀▀ ▀ ▀▀')
+print('\n')
+print(Fore.RED + 'You password:' + Style.RESET_ALL + password)
+os.system("pause")
